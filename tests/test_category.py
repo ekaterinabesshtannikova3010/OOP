e223ebf46ac67ed5ecf1d4f4aa36dade6502b2ea
ekaterina besshtannikova <dolmatova3010@yandex.ru>
2024-08-07 16:31:11 +0700

    Д/З № 15.1

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,5 +1,11 @@
 import pytest
 from src.product import Product
+from src.category import Category
+
+
+@pytest.fixture
+def create_product():
+    return Product("Test Product", "Test Description", 10, 5)
 
 
 def test_product_price_setter_positive():
@@ -12,6 +18,11 @@ def test_product_price_setter_negative():
     product = Product("Test", "Description", 10, 100)
     product.price = -5
     assert product.price == 10
+
+
+def test_category_str_method(create_product):
+    category = Category("Category3", "Description3", [create_product])
+    assert str(category) == "Название категории: Category3, количество продуктов: 1 шт."
 
 
 if __name__ == '__main__':
--- tests/test_product.py
+++ tests/test_product.py
@@ -2,6 +2,11 @@ from src.product import Product
 import pytest
 
 
+@pytest.fixture
+def create_product():
+    return Product("Test Product", "Test Description", 10, 5)
+
+
 def test_price_setter_negative():
     product = Product("Test Product", "Description", 10.0, 5)
     product.price = -5
@@ -18,6 +23,13 @@ def test_price_setter_positive():
     product = Product("Test Product", "Description", 10.0, 5)
     product.price = 15.0
     assert product.price == 15.0
+
+
+def test_product_add_method():
+    product1 = Product("Product1", "Description1", 20, 3)
+    product2 = Product("Product2", "Description2", 15, 2)
+    total_cost = product1 + product2
+    assert total_cost == 90
 
 
 if __name__ == "__main__":
